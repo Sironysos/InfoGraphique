dd06da21669843c8f2fb03f3bab952a2cd91cb2f
Alice Lombard <alice.lombard@insa-rennes.fr>
2024-11-25 11:57:48 +0100

    models train final + start animation

--- sampleProject/projet.cpp
+++ sampleProject/projet.cpp
@@ -22,10 +22,18 @@ void initialize_scene( Viewer& viewer )
 
 
 	//Frame
-    /* FrameRenderablePtr frame = std::make_shared<FrameRenderable>(flatShader);
-    viewer.addRenderable(frame); */
+    FrameRenderablePtr frame = std::make_shared<FrameRenderable>(flatShader);
+    viewer.addRenderable(frame);
 
-    //Animated train
+    //Final train
+    const std::string train_path = "../../models3D/3d-model.obj";
+    MeshRenderablePtr train = std::make_shared<MeshRenderable>(flatShader, train_path);
+    train->setGlobalTransform(glm::mat4(1.0));
+    //scale the train
+    train->setLocalTransform(getScaleMatrix(0.01,0.01,0.01));
+    viewer.addRenderable(train);
+
+    /* //Animated sick train
     const std::string train_path = "../../models3D/traingMalaaade.obj";
     MeshRenderablePtr train = std::make_shared<MeshRenderable>(flatShader, train_path);
     train->setGlobalTransform(glm::mat4(1.0));
@@ -37,21 +45,28 @@ void initialize_scene( Viewer& viewer )
     // Keyframes on parent transformation
     train->addGlobalTransformKeyframe(getRotationMatrix(0.0, glm::vec3(0,1,0)),0.0);
     train->addGlobalTransformKeyframe(getRotationMatrix(M_PI*0.25, glm::vec3(0,1,0)),2.0);
-    /* train->addGlobalTransformKeyframe(getRotationMatrix(M_PI*0.5, glm::vec3(0,1,0)), 2.0);
-    train->addGlobalTransformKeyframe(getRotationMatrix(M_PI*0.25, glm::vec3(0,1,0)), 3.0); */
     train->addGlobalTransformKeyframe(getRotationMatrix(M_PI*0.0, glm::vec3(0,1,0)), 4.0);
     train->addGlobalTransformKeyframe(getRotationMatrix(-M_PI*0.25, glm::vec3(0,1,0)), 6.0);
-    /* train->addGlobalTransformKeyframe(getRotationMatrix(-M_PI*0.5, glm::vec3(0,1,0)), 6.0);
-    train->addGlobalTransformKeyframe(getRotationMatrix(-M_PI*0.25, glm::vec3(0,1,0)), 7.0); */
-    train->addGlobalTransformKeyframe(getRotationMatrix(M_PI*0.0, glm::vec3(0,1,0)), 8.0);
+    train->addGlobalTransformKeyframe(getRotationMatrix(M_PI*0.0, glm::vec3(0,1,0)), 8.0); */
 
-    viewer.startAnimation();
+    /* //Animated box
+    const std::string box_path = "../../models3D/caisse.obj";
+    MeshRenderablePtr box = std::make_shared<MeshRenderable>(flatShader, box_path);
+    box->setGlobalTransform(glm::mat4(1.0));
+    
+    viewer.addRenderable(box);
+
+    // Keyframes on parent transformation
+    box->addGlobalTransformKeyframe(getTranslationMatrix(0,0,0),0.0);
+    box->addGlobalTransformKeyframe(getTranslationMatrix(0,0,-4.25),8.0);
+
+    viewer.startAnimation(); */
 
 }
 
 int main() 
 {
-    glm::vec4 background_color(0.0,0.0,0.0,1);
+    glm::vec4 background_color(0.8,0.8,0.8,1);
 	Viewer viewer(1280,720, background_color);
 	initialize_scene(viewer);
 
