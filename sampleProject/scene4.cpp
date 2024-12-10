0895e8c1c0004ccbe4fd702c88d3008bfba080c3
Alice Lombard <alice.lombard@insa-rennes.fr>
2024-12-10 15:20:28 +0100

    Envie de crever (on voit pas les textures du cactus ni la lumière)

--- sampleProject/scene4.cpp
+++ sampleProject/scene4.cpp
@@ -1,26 +1,41 @@
 #include <Viewer.hpp>
 #include <ShaderProgram.hpp>
+#include <CylinderMeshRenderable.hpp>
 #include <MeshRenderable.hpp>
 #include <FrameRenderable.hpp>
-#include "./../sfmlGraphicsPipeline/include/texturing/TexturedMeshRenderable.hpp"
-#include <Utils.hpp>
-#include <Io.hpp>
+#include <MeshRenderable.hpp>
 
-#include <lighting/LightedMeshRenderable.hpp>
-#include <lighting/Light.hpp>
+#include <texturing/TexturedMeshRenderable.hpp>
+#include <texturing/TexturedLightedMeshRenderable.hpp>
+#include <Io.hpp>
+#include <lighting/SpotLightRenderable.hpp>
 
 void initialize_scene( Viewer& viewer )
 {
     // In this scene, we will see the penguin panic next to his lever
     
-    // Create a shader program
-	ShaderProgramPtr phongShader = std::make_shared<ShaderProgram>(
-        "../../sfmlGraphicsPipeline/shaders/phongVertex.glsl",
-        "../../sfmlGraphicsPipeline/shaders/phongFragment.glsl");
+    
+    //Default shader
+    ShaderProgramPtr flatShader = std::make_shared<ShaderProgram>(  "../../sfmlGraphicsPipeline/shaders/flatVertex.glsl",
+                                                                    "../../sfmlGraphicsPipeline/shaders/flatFragment.glsl");
+    viewer.addShaderProgram( flatShader );
 
-    // Add the shader program to the viewer
+    //Define a shader that encode an illumination model
+    ShaderProgramPtr phongShader = std::make_shared<ShaderProgram>( "../../sfmlGraphicsPipeline/shaders/phongVertex.glsl", 
+                                                                    "../../sfmlGraphicsPipeline/shaders/phongFragment.glsl");
     viewer.addShaderProgram( phongShader );
 
+    //Add a 3D frame to the viewer
+    FrameRenderablePtr frame = std::make_shared<FrameRenderable>(flatShader);
+    viewer.addRenderable(frame);
+
+    //Textured shader
+    //    ShaderProgramPtr texShader = std::make_shared<ShaderProgram>("../shaders/textureVertex.glsl","../shaders/textureFragment.glsl");
+    ShaderProgramPtr texShader = std::make_shared<ShaderProgram>(   "../../sfmlGraphicsPipeline/shaders/textureVertex.glsl",
+                                                                    "../../sfmlGraphicsPipeline/shaders/textureFragment.glsl");
+    viewer.addShaderProgram( texShader );
+
+    /* //Lights
     glm::vec3 dir = glm::normalize(glm::vec3(-1,-1,-1));
     glm::vec3 ambient = glm::vec3(0,0,0);
     glm::vec3 diffuse = glm::vec3(1,1,1);
@@ -39,17 +54,26 @@ void initialize_scene( Viewer& viewer )
 
     viewer.addDirectionalLight(light1);
     viewer.addDirectionalLight(light2);
-    viewer.addDirectionalLight(light3);
+    viewer.addDirectionalLight(light3); */
 
-	//Frame
-    FrameRenderablePtr frame = std::make_shared<FrameRenderable>(phongShader);
-    viewer.addRenderable(frame);
+    // Add light to the scene
+	glm::vec3 red(0.9,0.3,0.4), green(0.3,0.9,0.4), blue(0.4,0.3,0.9);
+	glm::vec3 white(1,1,1);
+	glm::vec3 leafGreen(0,1,0);
+	{ // Moving SpotLight
+        auto spot_light = std::make_shared<SpotLight>(glm::vec3(0,3,0), glm::vec3(0,-0.2,-0.1), glm::vec3(0), white, glm::vec3(0), 1, 0, 0, 0.98, 0.92);
+        viewer.addSpotLight(spot_light);
 
-    //Rusty train
+        auto spot_light_renderable = std::make_shared<SpotLightRenderable>(phongShader, spot_light);
+        viewer.addRenderable(spot_light_renderable);
+    }
 
-    //Final train
-    const std::string train_path = "../../models3D/rustyTrain.obj";
-    const std::string mtl_basepath = "../../models3D/";
+    // Textured cactus
+    viewer.getCamera().setViewMatrix( glm::lookAt( glm::vec3(0, 0, 2 ), glm::vec3(0, 0, 0), glm::vec3( 0, 1, 0 ) ) );
+    std::string bunny_mesh_path = "../../models3D/cactus/source/untitled.obj";
+    std::string bunny_texture_path = "../../models3D/cactus/textures/Amigo_03.png";
+
+    std::string bunny_mesh_path2 = "../../models3D/cactus/source/cap.obj";
 
     std::vector<std::vector<glm::vec3>> all_positions;
     std::vector<std::vector<glm::vec3>> all_normals;
@@ -57,38 +81,16 @@ void initialize_scene( Viewer& viewer )
     std::vector<std::vector<unsigned int>> all_indices;
     std::vector<MaterialPtr> materials;
 
-    read_obj_with_materials(train_path, mtl_basepath, all_positions, all_normals, all_texcoords, materials);
-
-    int n_object = materials.size();
-    std::vector<glm::vec4> colors;
-
-    LightedMeshRenderablePtr train;
-
-    train = std::make_shared<LightedMeshRenderable>(phongShader, all_positions[0], all_normals[0], colors, materials[0]);
-    for (int i = 1 ; i < n_object ; ++i){
-        LightedMeshRenderablePtr part = std::make_shared<LightedMeshRenderable>(
-        phongShader, all_positions[i], all_normals[i], colors, materials[i]);
-        HierarchicalRenderable::addChild(train, part);
-        
-    }
+    read_obj_with_materials(bunny_mesh_path, "../../models3D/cactus/source/", all_positions, all_normals, all_texcoords, materials);
+    TexturedLightedMeshRenderablePtr bunny = std::make_shared<TexturedLightedMeshRenderable>(texShader, bunny_mesh_path, materials[0], bunny_texture_path);
     
-    train->setGlobalTransform(glm::mat4(1.0));
-    //scale the train
-    train->setLocalTransform(getScaleMatrix(1,1,1));
-    viewer.addRenderable(train);
+    viewer.addRenderable(bunny);
 
-    /* //Animated box
-    const std::string box_path = "../../models3D/caisse.obj";
-    TexturedMeshRenderablePtr box = std::make_shared<TexturedMeshRenderable>(phongShader, box_path, "../../textures/cardboard.jpg");
-    box->setGlobalTransform(glm::mat4(1.0));
+    read_obj_with_materials(bunny_mesh_path2, "../../models3D/cactus/source/", all_positions, all_normals, all_texcoords, materials);
+    TexturedLightedMeshRenderablePtr bunny2 = std::make_shared<TexturedLightedMeshRenderable>(texShader, bunny_mesh_path2, materials[0], bunny_texture_path);
     
-    viewer.addRenderable(box);
+    viewer.addRenderable(bunny2);
 
-    // Keyframes on parent transformation
-    box->addGlobalTransformKeyframe(getTranslationMatrix(0,0,0),0.0);
-    box->addGlobalTransformKeyframe(getTranslationMatrix(0,0,-4.25),8.0); */
-
-    viewer.startAnimation();
 
 }
 
