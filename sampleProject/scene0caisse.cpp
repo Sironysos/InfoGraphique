6da8727ef725d4c1f610e763d67af46120dbaaca
Alice Lombard <alice.lombard@insa-rennes.fr>
2024-12-13 19:12:21 +0100

    cleaned scene0caisse

--- sampleProject/scene0caisse.cpp
+++ sampleProject/scene0caisse.cpp
@@ -1,43 +1,22 @@
 #include <Viewer.hpp>
 #include <ShaderProgram.hpp>
-#include <FrameRenderable.hpp>
-#include <Utils.hpp>
 #include <lighting/SpotLightRenderable.hpp>
-#include <lighting/LightedMeshRenderable.hpp>
 #include <texturing/TexturedLightedMeshRenderable.hpp>
 #include <Io.hpp>
 
 void initialize_scene( Viewer& viewer )
 {
-    //Textured shader
-    //    ShaderProgramPtr texShader = std::make_shared<ShaderProgram>("../shaders/textureVertex.glsl","../shaders/textureFragment.glsl");
+    //Shaders
     ShaderProgramPtr texShader = std::make_shared<ShaderProgram>(   "../../sfmlGraphicsPipeline/shaders/textureVertex.glsl",
                                                                     "../../sfmlGraphicsPipeline/shaders/textureFragment.glsl");
-    viewer.addShaderProgram( texShader );
+    viewer.addShaderProgram(texShader);
 
-    ShaderProgramPtr phongShader = std::make_shared<ShaderProgram>(
-        "../../sfmlGraphicsPipeline/shaders/phongVertex.glsl",
-        "../../sfmlGraphicsPipeline/shaders/phongFragment.glsl");
+    ShaderProgramPtr phongShader = std::make_shared<ShaderProgram>( "../../sfmlGraphicsPipeline/shaders/phongVertex.glsl",
+                                                                    "../../sfmlGraphicsPipeline/shaders/phongFragment.glsl");
     viewer.addShaderProgram(phongShader);
 
-    // Define colors for lights
-    glm::vec3 yellow = glm::vec3(1, 1, 0);
+    // Add light to the scene
     glm::vec3 white(1,1,1);
-
-    /* { // SpotLight
-        // Set the initial Spotlight position inside the box
-        auto spot_light = std::make_shared<SpotLight>(glm::vec3(0,3,-2), glm::vec3(0,-1,3), glm::vec3(0), white, glm::vec3(0), 1, 0, 0, 0.98, 0.92);
-        viewer.addSpotLight(spot_light);
-
-        auto spot_light_renderable = std::make_shared<SpotLightRenderable>(phongShader, spot_light);
-        viewer.addRenderable(spot_light_renderable);
-
-        spot_light_renderable->setLocalTransform(getScaleMatrix(0.1, 0.1, 0.1));
-
-        // Animate the spotlight by adding keyframes for its position*
-        spot_light->addGlobalTransformKeyframe(lookAtModel(glm::vec3(0,5,-6), glm::vec3(0,0,0), Light::base_forward), 0);
-        //spot_light->addGlobalTransformKeyframe(lookAtModel(glm::vec3(0,5,-10.25), glm::vec3(0,0,-2), Light::base_forward), 10.0);
-    } */
     { // SpotLight
         auto spot_light = std::make_shared<SpotLight>(glm::vec3(0,4,8), glm::vec3(0,0.2,0.2), glm::vec3(0), white, glm::vec3(0), 1, 0, 0, 0.98, 0.92);
         viewer.addSpotLight(spot_light);
@@ -48,7 +27,6 @@ void initialize_scene( Viewer& viewer )
 
     //Camera movement
     viewer.getCamera().setViewMatrix(glm::lookAt(glm::vec3(0, 1, 3), glm::vec3(0, -0.5, 0), glm::vec3( 0, 1, 0 ) ) );
-
 
     const std::string box_path = "../../models3D/caisse.obj";
     std::string box_texture_path = "../../models3D/crate.jpg";
@@ -63,7 +41,6 @@ void initialize_scene( Viewer& viewer )
     TexturedLightedMeshRenderablePtr box = std::make_shared<TexturedLightedMeshRenderable>(texShader, box_path, materials[0], box_texture_path);
     box->setGlobalTransform(getTranslationMatrix(0, -1, -1));
 
-
     // Keyframe animation for the box movement
     box->addGlobalTransformKeyframe(getTranslationMatrix(0, -1, -1), 0.0);
     box->addGlobalTransformKeyframe(getTranslationMatrix(0, -1, -1)*getTranslationMatrix(0, 0, 1), 40.0);
@@ -73,7 +50,6 @@ void initialize_scene( Viewer& viewer )
 int main() 
 {
     glm::vec4 background_color(0.0,0.0,0.0,1);
-	//glm::vec4 background_color(0.8,0.8,0.8,1);
 	Viewer viewer(1280,720, background_color);
 	initialize_scene(viewer);
 	viewer.startAnimation();
@@ -85,6 +61,5 @@ int main()
 		viewer.draw();
 		viewer.display();
 	}	
-
 	return EXIT_SUCCESS;
 }
