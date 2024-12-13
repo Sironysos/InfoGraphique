dbd283ba359f5e178a62782592ade298940a6f7b
Alice Lombard <alice.lombard@insa-rennes.fr>
2024-12-13 18:29:36 +0100

    cleaned scene1

--- sampleProject/scene0malaaade.cpp
+++ sampleProject/scene0malaaade.cpp
@@ -8,7 +8,6 @@
 #include <lighting/SpotLightRenderable.hpp>
 #include <lighting/DirectionalLightRenderable.hpp>
 #include <lighting/LightedMeshRenderable.hpp>
-#include <texturing/CubeMapRenderable.hpp>
 #include <lighting/LightedCubeRenderable.hpp>
 
 void initialize_scene( Viewer& viewer )
@@ -19,25 +18,13 @@ void initialize_scene( Viewer& viewer )
         "../../sfmlGraphicsPipeline/shaders/flatFragment.glsl");
 	viewer.addShaderProgram( flatShader );
 
-	ShaderProgramPtr cubeMapShader = std::make_shared<ShaderProgram>(  "../../sfmlGraphicsPipeline/shaders/cubeMapVertex.glsl",
-                                                                    "../../sfmlGraphicsPipeline/shaders/cubeMapFragment.glsl");
-	viewer.addShaderProgram(cubeMapShader);
-
 	//Define a shader that encode an illumination model
     ShaderProgramPtr phongShader = std::make_shared<ShaderProgram>( "../../sfmlGraphicsPipeline/shaders/phongVertex.glsl", 
                                                                     "../../sfmlGraphicsPipeline/shaders/phongFragment.glsl");
     viewer.addShaderProgram( phongShader );
 
-	auto mat = std::make_shared<Material>(glm::vec3(0), glm::vec3(1), glm::vec3(0), 100.0f);
-
-    
-
-	//Define a transformation
-    glm::mat4 globalTransformation, localTransformation;
 
 	// Add light to the scene
-	glm::vec3 red(0.9,0.3,0.4), green(0.3,0.9,0.4), blue(0.4,0.3,0.9);
-	glm::vec3 white(1,1,1);
 	glm::vec3 maladWhite(1,0.933,0.64);
 	{ // SpotLight
         auto spot_light = std::make_shared<SpotLight>(glm::vec3(0,3,-8), glm::vec3(0,-0.2,-0.1), glm::vec3(0), maladWhite, glm::vec3(0) ,1, 0, 0, 0.98, 0.92);
@@ -47,28 +34,28 @@ void initialize_scene( Viewer& viewer )
         viewer.addRenderable(spot_light_renderable);
     }
 
-
 	const std::string traing_path = "../../models3D/traingMalaaade.obj";
     LightedMeshRenderablePtr traing = std::make_shared<LightedMeshRenderable>(phongShader, traing_path, Material::GreenRubber());
 	viewer.addRenderable(traing);
 
+	auto mat = std::make_shared<Material>(glm::vec3(0), glm::vec3(1), glm::vec3(0), 100.0f);
     auto lighted_cube = std::make_shared<LightedCubeRenderable>(phongShader, false, mat);
 
 	HierarchicalRenderable::addChild(traing, lighted_cube);
 
 	traing->setGlobalTransform(glm::mat4(1.0)*getRotationMatrix(-M_PI*0.55, glm::vec3(1,0,0))*getTranslationMatrix(glm::vec3(0,0,-2)));
 
-	traing->addLocalTransformKeyframe(getRotationMatrix(M_PI*0.0, glm::vec3(0,0,1)),0.0/0.3);
-	traing->addLocalTransformKeyframe(getRotationMatrix(M_PI/6, glm::vec3(0,0,1)),1.0/0.3);
-	traing->addLocalTransformKeyframe(getRotationMatrix(M_PI/3, glm::vec3(0,0,1)),2.0/0.3);
-	traing->addLocalTransformKeyframe(getRotationMatrix(M_PI/2, glm::vec3(0,0,1)),3.0/0.3);
-	traing->addLocalTransformKeyframe(getRotationMatrix(M_PI*2/3, glm::vec3(0,0,1)),4.0/0.3);
-	traing->addLocalTransformKeyframe(getRotationMatrix(M_PI*5/6, glm::vec3(0,0,1)),5.0/0.3);
-	traing->addLocalTransformKeyframe(getRotationMatrix(M_PI*2/3, glm::vec3(0,0,1)),6.0/0.3);
-	traing->addLocalTransformKeyframe(getRotationMatrix(M_PI/2, glm::vec3(0,0,1)),7.0/0.3);
-	traing->addLocalTransformKeyframe(getRotationMatrix(M_PI/3, glm::vec3(0,0,1)),8.0/0.3);
-	traing->addLocalTransformKeyframe(getRotationMatrix(M_PI/6, glm::vec3(0,0,1)),9.0/0.3);
-	traing->addLocalTransformKeyframe(getRotationMatrix(M_PI*0.0, glm::vec3(0,0,1)),10.0/0.3);
+	traing->addLocalTransformKeyframe(glm::vec3(0),0/0.3);
+	traing->addLocalTransformKeyframe(getRotationMatrix(M_PI/6, glm::vec3(0,0,1)),1/0.3);
+	traing->addLocalTransformKeyframe(getRotationMatrix(M_PI/2, glm::vec3(0,0,1)),3/0.3);
+	traing->addLocalTransformKeyframe(getRotationMatrix(M_PI/3, glm::vec3(0,0,1)),2/0.3);
+	traing->addLocalTransformKeyframe(getRotationMatrix(M_PI*2/3, glm::vec3(0,0,1)),4/0.3);
+	traing->addLocalTransformKeyframe(getRotationMatrix(M_PI*5/6, glm::vec3(0,0,1)),5/0.3);
+	traing->addLocalTransformKeyframe(getRotationMatrix(M_PI*2/3, glm::vec3(0,0,1)),6/0.3);
+	traing->addLocalTransformKeyframe(getRotationMatrix(M_PI/2, glm::vec3(0,0,1)),7/0.3);
+	traing->addLocalTransformKeyframe(getRotationMatrix(M_PI/3, glm::vec3(0,0,1)),8/0.3);
+	traing->addLocalTransformKeyframe(getRotationMatrix(M_PI/6, glm::vec3(0,0,1)),9/0.3);
+	traing->addLocalTransformKeyframe(glm::vec3(0),10/0.3);
 	
 	lighted_cube->setGlobalTransform(glm::mat4(1.0)*getRotationMatrix(-M_PI*0.55, glm::vec3(1,0,0))*getTranslationMatrix(glm::vec3(0,0,-2)));
     lighted_cube->setLocalTransform(getScaleMatrix(50,.2,50)*getTranslationMatrix(0,0,0));
