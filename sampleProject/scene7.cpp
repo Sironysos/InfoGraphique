c644f3ed1a41bbcff3bc414a9554f3a0714ba37c
Noraid <dorian.nowaczyk@insa-rennes.fr>
2024-12-13 16:39:03 +0100

    Début particules scène 7

--- sampleProject/scene7.cpp
+++ sampleProject/scene7.cpp
@@ -19,6 +19,9 @@
 
 #include <dynamics/DynamicSystem.hpp>
 #include <dynamics/EulerExplicitSolver.hpp>
+#include <dynamics/DynamicSystemRenderable.hpp>
+#include <dynamics/ConstantForceField.hpp>
+#include <dynamics/ParticleRenderable.hpp>
 
 void initialize_scene( Viewer& viewer )
 {
@@ -35,11 +38,58 @@ void initialize_scene( Viewer& viewer )
                                                                     "../../sfmlGraphicsPipeline/shaders/phongFragment.glsl");
     viewer.addShaderProgram( phongShader );
 
+    //Particle Shader
+    ShaderProgramPtr particleShader = std::make_shared<ShaderProgram>( "../../sfmlGraphicsPipeline/shaders/billboardVertex.glsl",
+                                                                        "../../sfmlGraphicsPipeline/shaders/billboardFragment.glsl");
+
     //Initialize a dynamic system (Solver, Time step, Restitution coefficient)
     DynamicSystemPtr system = std::make_shared<DynamicSystem>();
     EulerExplicitSolverPtr solver = std::make_shared<EulerExplicitSolver>();
     system->setSolver(solver);
     system->setDt(0.01);
+
+    //Create a renderable associated to the dynamic system
+    //This renderable is responsible for calling DynamicSystem::computeSimulationStep() in the animate() function
+    //It is also responsible for some of the key/mouse events
+    DynamicSystemRenderablePtr systemRenderable = std::make_shared<DynamicSystemRenderable>(system);
+    viewer.addRenderable(systemRenderable);
+
+    //Deactivate collision detection
+    system->setCollisionsDetection(false);
+
+    glm::vec3 px,pv;
+    float pm, pr;
+
+    {
+        //Initialize a particle with position, velocity, mass and radius and add it to the system
+        px = glm::vec3(0.0,1.0,0.0);
+        pv = glm::vec3(3.0,0.0,0.0);
+        pr = 0.1;
+        pm = 1.0;
+        ParticlePtr particle1 = std::make_shared<Particle>( px, pv, pm, pr);
+        system->addParticle( particle1 );
+
+        px = glm::vec3(0.0,1.5,0.0);
+        pv = glm::vec3(6.0,0.0,0.0);
+        pr = 0.1;
+        pm = 1.0;
+        ParticlePtr particle2 = std::make_shared<Particle>( px, pv, pm, pr);
+        system->addParticle( particle2 );
+
+        //Initialize a force field that apply to all the particles of the system to simulate gravity
+        //Add it to the system as a force field
+        ConstantForceFieldPtr gravityForceField = std::make_shared<ConstantForceField>(system->getParticles(), DynamicSystem::gravity );
+        system->addForceField( gravityForceField );
+
+        //Create a particleRenderable for each particle of the system
+        //DynamicSystemRenderable act as a hierarchical renderable
+        //This which allows to easily apply transformation on the visualiazation of a dynamicSystem
+        ParticleRenderablePtr particleRenderable1 = std::make_shared<ParticleRenderable>(particleShader, particle1);
+        HierarchicalRenderable::addChild( systemRenderable, particleRenderable1 );
+        ParticleRenderablePtr particleRenderable2 = std::make_shared<ParticleRenderable>(particleShader, particle2);
+        HierarchicalRenderable::addChild( systemRenderable, particleRenderable2 );
+    }
+
     
     //Lights
 	glm::vec3 dir = glm::normalize(glm::vec3(-1,-1,-1));
